2cc635de48fd409697a251550772576410ac8b0d
pkthasin <pkthasin@gmail.com>
2023-01-18 22:26:16 +0900

    등산코스 vo 중간세이브

---
+++ src/main/java/com/yummyclimbing/vo/course/CourseValuesVO.java
@@ -0,0 +1,11 @@
+package com.yummyclimbing.vo.course;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class CourseValuesVO {
+	CourseAttributeVO attributes;
+	CouresGeometryVO geometry;
+}
