6d56b9817f7cb8fd0d41b14f5312b6ed875fba5f
ssong <ssongyg113@naver.com>
2023-01-19 12:44:10 +0900

    partyVO수정

--- src/main/java/com/yummyclimbing/vo/party/PartyBoardVO.java
+++ src/main/java/com/yummyclimbing/vo/party/PartyBoardVO.java
@@ -5,11 +5,15 @@ import lombok.Data;
 @Data
 public class PartyBoardVO {
 
-	private int pbNum;				//소모임 게시글 번호(프라임키)
-	private String pbComment;	//소모임 댓글
-	private String pbCredat;		//소모임 게시글 생성일
-	private String pbModdat;		//소모임 게시글 수정일
+	private int pbNum;				//소모임 게시물 번호(프라임키)
+	private String pbContent;		//소모임 게시물 내용
+	private String pbCredat;		//소모임 게시물 생성일
+	private String pbLmodat;		//소모임 게시물 수정일
+	private String pbCretim;		//소모임 게시물 생성시간
+	private String pbLmotim;		//소모임 게시물 수정시간
+	private int pbActive;			//소모임 게시물 등록시 1-> 삭제시0
+	
 	private int piNum;				//소모임 번호(외래키)
 	private int uiNum;				//유저 번호(외래키)
-	private String uiId;				//유저 아이디(외래키)	
+	private String uiNickname;		//유저 닉네임	
 }
