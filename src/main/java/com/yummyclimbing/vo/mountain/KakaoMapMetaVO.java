ba069598d25197ee634bad76e2e089a5ddf62c0b
Taeshin Park <pkthasin@gmail.com>
2023-02-10 10:53:40 +0900

    헤더 수정

--- src/main/java/com/yummyclimbing/vo/mountain/KakaoMapMetaVO.java
+++ src/main/java/com/yummyclimbing/vo/mountain/KakaoMapMetaVO.java
@@ -10,6 +10,6 @@ public class KakaoMapMetaVO {
     private int pageable_count;
     private int total_count;
     
-    @JsonProperty("is_end")
+    @JsonProperty("is_end") // jackson에서 변환할 때 변수명 is를 인식하지 못해서 따로 지정해줬음
     private boolean is_end;
 }
