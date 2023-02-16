795998a47b1ede180ecc98d19dae4aa934da5625
Umin <adorecamus@gmail.com>
2023-02-16 10:25:31 +0900

    다시 수정..

--- src/main/java/com/yummyclimbing/config/HttpConfig.java
+++ src/main/java/com/yummyclimbing/config/HttpConfig.java
@@ -5,6 +5,7 @@ import org.apache.catalina.connector.Connector;
 import org.apache.tomcat.util.descriptor.web.SecurityCollection;
 import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,7 +13,7 @@ import org.springframework.context.annotation.Configuration;
 public class HttpConfig {
 
 	@Bean
-	public TomcatServletWebServerFactory servletContainer() {
+	public ServletWebServerFactory servletContainer() {
 		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
 			@Override
 			protected void postProcessContext(Context context) {
@@ -28,7 +29,7 @@ public class HttpConfig {
 		tomcat.addAdditionalTomcatConnectors(createSslConnector());
 		return tomcat;
 	}
-	
+
 	private Connector createSslConnector() {
 		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
 		connector.setPort(80);
