6144b30b86df332131b1ea0c45950cc56a782ac5
Umin <adorecamus@gmail.com>
2023-02-16 10:07:34 +0900

    수정

--- src/main/java/com/yummyclimbing/config/HttpConfig.java
+++ src/main/java/com/yummyclimbing/config/HttpConfig.java
@@ -5,7 +5,6 @@ import org.apache.catalina.connector.Connector;
 import org.apache.tomcat.util.descriptor.web.SecurityCollection;
 import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,7 +12,7 @@ import org.springframework.context.annotation.Configuration;
 public class HttpConfig {
 
 	@Bean
-	public ServletWebServerFactory servletContainer() {
+	public TomcatServletWebServerFactory servletContainer() {
 		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
 			@Override
 			protected void postProcessContext(Context context) {
